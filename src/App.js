ee00987b373a9e524d4e30804dc752c523ef9185
Şeref Kaynak <serefkynk@gmail.com>
2023-10-22 00:17:50 +0300

    Fetch Data from NASA API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+
 import "./App.css";
 
+const API_KEY = "OrG448C73BjcAit6gEXRdkjk4vRqDaZo8zJDZrzg";
+
+
 function App() {
+
+  const [nasaData, setNasaData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => setNasaData(data));
+      console.log(nasaData);
+  },[]);
+
   return (
     <div className="App">
       <p>
         NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
 		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      {nasaData && <img src={nasaData.url} alt={nasaData.title} />}
+      {nasaData && <p>{nasaData.explanation}</p>}
+      {nasaData && <p>{nasaData.date}</p>}
     </div>
   );
 }
