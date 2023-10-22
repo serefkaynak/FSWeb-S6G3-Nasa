77e666bc4e147d99e5583206cab77306873a479b
Şeref Kaynak <serefkynk@gmail.com>
2023-10-22 23:04:45 +0300

    Header and Footer added.

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,27 +2,29 @@ import React from 'react';
 import './Content.css';
 
 function LoadingComponent() {
-    return <h1>Loading...</h1>;
+    return <h1>Nasa'ya Bağlanıyor...</h1>;
 }
 
 function ContentTitleComponent(){
     return <h1>Astronomy Photo Of The Day</h1>;
 }
 
-function ContentDescriptionComponent(){
-    return <p>Click on the image to enlarge</p>;
+function ContentDescriptionComponent(props){
+    return <div 
+    className="content">
+    <ContentTitleComponent />
+    {props.nasaData && <img src={props.nasaData.url} alt={props.nasaData.title} className="nasa-image" />}
+    {props.nasaData && <h1>{props.nasaData.title}</h1>}
+    {props.nasaData && <p>{props.nasaData.explanation}</p>}
+    {props.nasaData && <p>Date: {props.nasaData.date}</p>}
+        </div>;
 }
 
 function Content(props) {
     return (
-        !props.nasaData === true ? <LoadingComponent /> :
-        <div className="content">
-            <ContentTitleComponent />
-            {props.nasaData && <img src={props.nasaData.url} alt={props.nasaData.title} className="nasa-image" />}
-            {props.nasaData && <h1>{props.nasaData.title}</h1>}
-            {props.nasaData && <p>{props.nasaData.explanation}</p>}
-            {props.nasaData && <p>Date: {props.nasaData.date}</p>}
-        </div>
+        !props.nasaData === true 
+        ? <LoadingComponent /> 
+        : <ContentDescriptionComponent />
     );
 }
 
